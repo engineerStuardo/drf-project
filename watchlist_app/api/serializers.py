98fc679313b4a67b8ef0b868ff74e140c8541bf1
Italo Stuardo <italostuardom3@Italos-MacBook-Pro-3.local>
2025-05-19 04:08:52 -0600

    Using Field validator, Object validator and also just validator (this is ment to be reusable for other fields)

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 
 from watchlist_app.models import Movie
 
+# VALIDATOR EXAMPLE ALMOST SAME AS FIELD VALIDATION
+def name_length_validator(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short.")
+    return value
+
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length_validator])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -18,6 +24,19 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    # FIELD VALIDATION EXAMPLE
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name must be greater than 2 characters.")
+    #     return value
+
+    # OBJECT VALIDATION EXAMPLE
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and Description cannot be the same.")
+        return data
+
 
 
 
