5cb67fd1bf151bdfe0a8f77f33d955a777c1a459
Italo Stuardo <italostuardom3@Italos-MacBook-Pro-3.local>
2025-05-21 05:59:32 -0600

    Working with ViewSets

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,16 +1,25 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from watchlist_app.api.views import (WatchListView, WatchListDetailView,
-                                     StreamPlatformListView, StreamPlatformDetailView,
-                                     ReviewList, ReviewDetail, ReviewCreate)
+                                     ReviewList, ReviewDetail, ReviewCreate,
+                                     StreamPlatformViewSet)
+
+
+router = DefaultRouter()
+router.register(r'stream', StreamPlatformViewSet, basename='stream_platform')
+
 
 urlpatterns = [
     # Watch APIS
     path('list/', WatchListView.as_view(), name='watch_list'),
     path('<int:pk>/', WatchListDetailView.as_view(), name='watch_list_detail'),
 
-    # Platform APIS
-    path('stream/', StreamPlatformListView.as_view(), name='stream_platform_list'),
-    path('stream/<int:pk>/', StreamPlatformDetailView.as_view(), name='stream_platform_detail'),
+    # Platform APIS:
+    # Including this url in the root urlpatterns
+    # /watch/stream/
+    # /watch/stream/<pk>/
+    path('', include(router.urls)),
 
     # Review APIS
     path('stream/<int:pk>/review', ReviewList.as_view(), name='review_list'),
